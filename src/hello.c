ed3ea06e04015fc5595472a8393604e980416c60
Valdis Saulespurens <38938337+ValRCS@users.noreply.github.com>
2025-09-14 19:37:54 +0000

    Adjust formatting for hello world

--- src/hello.c
+++ src/hello.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello, RTU World from C Lab!\n");
+  // This is your first C program my friend
+  printf("Hello, RTU World from C Lab in 2025!\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
-    printf("  arg[%d] = %s\\n", i, argv[i]);
+    printf("  arg[%d] = %s\n", i, argv[i]);
   }
   return 0;
 }
